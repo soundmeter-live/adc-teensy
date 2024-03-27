f62976bda7db95922636fdb8f74324242cd5cede
YutongWen <128516053+YutongWen@users.noreply.github.com>
2024-03-27 16:32:12 -0400

    Update audio_in_teensy_2.ino

--- audio_in_teensy_2.ino
+++ audio_in_teensy_2.ino
@@ -9,3 +9,11 @@ AudioOutputUSB usb_to;
 AudioInputI2Sslave i2s_from;
 AudioConnection patchCord_in3(i2s_from, 0, usb_to ,0);
 AudioConnection patchCord_in4(i2s_from, 1, usb_to, 1)
+
+void setup() {
+  AudioMemory(128);
+}
+
+void loop(){
+
+}
